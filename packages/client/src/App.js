099ffca6d8829ebc462bd6e3c4bab8d3d7a7994d
sunnibfgi <ja1523@proton.me>
2023-02-15 18:34:14 +0800

    1

---
+++ packages/client/src/App.js
@@ -0,0 +1,14 @@
+import React, {useEffect} from 'react'
+
+function App() {
+  useEffect(() => {
+    alert('')
+  }, [])
+  return (
+    <div className="App">
+      hello world
+    </div>
+  );
+}
+
+export default App;
